59aa161eda23798c73894fa916b415af959c0e27
Mirlan <email>
2022-09-15 06:38:24 +0600

    created/develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { Transition } from "react-transition-group";
+import Home from "./pages/home/home";
 
 function App() {
-  const [stateMusubi, setStateMusubi] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setStateMusubi(false);
-    }, 2000000);
-  }, []);
-
   return (
     <>
-      <Transition in={stateMusubi} timeout={500} mountOnEnter unmountOnExit>
-        {(state) => (
-          <div className="musubi">
-            <div class="container">
-              <div class="glitch" data-text="Diamond">
-                Diamond
-              </div>
-              <div class="glow">Programming course</div>
-              <p class="subtitle">welcome to new world</p>
-            </div>
-            <div class="scanlines"></div>
-          </div>
-        )}
-      </Transition>
-
-      <h2>Musubi</h2>
+      <Home />
     </>
   );
 }
